8c1982ef5ba07030ea7cffb3b91e03805fe8d8a1
Jeongseup <seup87@gmail.com>
2023-04-28 14:22:04 +0900

    update helper.sh

--- gm/helper.sh
+++ gm/helper.sh
@@ -1,2 +1,6 @@
 # run local celestia
 docker run --platform linux/amd64 -p 26650:26657 -p 26659:26659 ghcr.io/rollkit/local-celestia-devnet:v0.9.1
+
+# query test
+curl -s -X GET http://0.0.0.0:26659/balance | jq
+
