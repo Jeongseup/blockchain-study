224974a2146607a0b27750286d0ab9382feb2658
Jeongseup <seup87@gmail.com>
2023-04-28 15:41:37 +0900

    add query text

--- gm/helper.sh
+++ gm/helper.sh
@@ -4,3 +4,5 @@ docker run --platform linux/amd64 -p 26650:26657 -p 26659:26659 ghcr.io/rollkit/
 # query test
 curl -s -X GET http://0.0.0.0:26659/balance | jq
 
+export KEY1=gm1ry7tppyaru9ul7ct88j93wg8uqxpgr8vyhjyka
+export KEY2=gm1l2zutn5ltjj2ussmfjkg87j9qlf3kmk3may53t
---
+++ gm/x/gm/keeper/query_gm.go
@@ -0,0 +1,23 @@
+package keeper
+
+import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"gm/x/gm/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (k Keeper) Gm(goCtx context.Context, req *types.QueryGmRequest) (*types.QueryGmResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// TODO: Process the query
+	_ = ctx
+
+	return &types.QueryGmResponse{}, nil
+}
